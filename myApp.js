b12d8b719c90d2a128a85b80a59f175d68a7f9b1
MOHAMMAD_ASAD <md12asad9@gmail.com>
2026-03-01 00:41:45 +0530

    static updated2.0

--- myApp.js
+++ myApp.js
@@ -1,9 +1,10 @@
 let express = require("express");
 let app = express();
 console.log(__dirname + "/views/index.html");
+app.use("/public",express.static(__dirname + "/public"));
+
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/views/index.html");
 });
-app.use(express.static(__dirname + "/public"));
 
 module.exports = app;
