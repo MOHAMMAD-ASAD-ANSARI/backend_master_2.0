4c5cf0a0849b9955d311a2d652085b990e9c34bc
MOHAMMAD_ASAD <md12asad9@gmail.com>
2026-02-28 06:30:56 +0530

    1st route

--- myApp.js
+++ myApp.js
@@ -1,43 +1,6 @@
-let express = require('express');
+let express = require("express");
 let app = express();
 console.log("hello world");
-app.get("/",function(req,res){
-    res.send('Response String');
+app.get("/", function (req, res) {
+    res.send("Hello Express");
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
