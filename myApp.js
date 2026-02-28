186d38d94fa7b8a5d5e8d7ef116784dbe0f23814
MOHAMMAD_ASAD <md12asad9@gmail.com>
2026-03-01 04:22:34 +0530

    mware

--- myApp.js
+++ myApp.js
@@ -6,6 +6,11 @@ app.get("/json", function(req,res){
 });
 app.use("/public",express.static(__dirname + "/public"));
 
+app.use(function(req,res,next){
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+})
+
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/views/index.html");
 });
