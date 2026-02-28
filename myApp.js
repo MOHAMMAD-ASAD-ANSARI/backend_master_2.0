115933803b59915bf1c09e1211d47778a73d0eec
MOHAMMAD_ASAD <md12asad9@gmail.com>
2026-03-01 00:33:21 +0530

    express.static

--- myApp.js
+++ myApp.js
@@ -4,5 +4,6 @@ console.log(__dirname + "/views/index.html");
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/views/index.html");
 });
+app.use(__dirname + "/public", express.static());
 
 module.exports = app;
