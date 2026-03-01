cb6a880b3f6ba0a5851c342b8302a82edbf373a0
MOHAMMAD_ASAD <md12asad9@gmail.com>
2026-03-02 00:33:28 +0530

    middleware in app.get

--- myApp.js
+++ myApp.js
@@ -1,15 +1,20 @@
 let express = require("express");
 let app = express();
 console.log(__dirname + "/views/index.html");
+app.use(function(req,res,next){
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+});
+app.get("/now", function(req,res,next){
+    req.time = new Date().toString();
+} , function(req,res){
+    res.send({"time" : req.time});
+});
 app.get("/json", function(req,res){
     res.json({"message": "Hello json"});
 });
 app.use("/public",express.static(__dirname + "/public"));
 
-app.use(function(req,res,next){
-    console.log(req.method + " " + req.path + " - " + req.ip);
-    next();
-})
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/views/index.html");
